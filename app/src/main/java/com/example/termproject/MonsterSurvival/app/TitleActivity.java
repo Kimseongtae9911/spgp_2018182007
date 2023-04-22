6f90ef52b7886c0c1413c3fa50ac01496c56a341
Kim Seongtae <seongtae07@gmail.com>
2023-04-22 21:11:47 +0900

    LobbyUI add

---
+++ app/src/main/java/com/example/termproject/MonsterSurvival/app/TitleActivity.java
@@ -0,0 +1,46 @@
+package com.example.termproject.MonsterSurvival.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.animation.ValueAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
+
+import com.example.termproject.R;
+import com.example.termproject.databinding.ActivityTitleBinding;
+
+public class TitleActivity extends AppCompatActivity {
+
+    private ActivityTitleBinding binding;
+
+    private ValueAnimator animator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityTitleBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        createAnimator();
+    }
+
+    private void createAnimator() {
+        animator = ValueAnimator.ofFloat(0.0f, 0.5f);
+        animator.setDuration(30000);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                float progress = (Float)valueAnimator.getAnimatedValue();
+                float tx = -1 * binding.backgroundImageView.getWidth() * progress;
+                binding.backgroundImageView.setTranslationX(tx);
+            }
+        });
+    }
+
+    public void onBtnStart(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+
+    }
+}
